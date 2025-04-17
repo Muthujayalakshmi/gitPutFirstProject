84c27ffab8f9ce24c0df1e8ec1d6eb0b35ed9f4f
Muthujayalakshmi <muthujayalakshmidata@gmail.com>
2025-04-17 16:19:01 +0530

    Changes done for checking code uodate on each branch

--- src/gitPractie/Hello.java
+++ src/gitPractie/Hello.java
@@ -3,6 +3,9 @@ package gitPractie;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Live in Real");
+		System.out.println("I have implemented folln activities");
+		System.out.println("Cloning Done");
+		System.out.println("Switched into Muthu Branch from Master");
 	}
 
 }
