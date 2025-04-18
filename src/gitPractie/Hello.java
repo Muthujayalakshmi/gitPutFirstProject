f3b80968c54f31366ce3bfde8b5f050e737f0a67
Muthujayalakshmi <muthujayalakshmidata@gmail.com>
2025-04-18 18:39:37 +0530

    Master class Work

--- src/gitPractie/Hello.java
+++ src/gitPractie/Hello.java
@@ -3,6 +3,7 @@ package gitPractie;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Live in Real");
+		System.out.println("I am updated for Master updateis availble but branch owner not hv master update");
 	}
 
 }
